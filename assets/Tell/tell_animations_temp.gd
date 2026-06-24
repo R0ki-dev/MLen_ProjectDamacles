b7063256969eb07728cce84d90e11549365fc262
Roki <64596787+R0ki-dev@users.noreply.github.com>
2026-06-23 18:03:15 -0700

    Added intro animation

--- assets/Tell/tell_animations_temp.gd
+++ assets/Tell/tell_animations_temp.gd
@@ -1,12 +1,24 @@
 extends Node3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+var IsPlayingIntro: bool = true
+
+func _on_ready() -> void:
+	animation_player.play("Tell/crouch_to_stand")
+	await $AnimationPlayer.animation_finished
+	IsPlayingIntro = false
 
 func WalkForward():
+	if IsPlayingIntro == true:
+		return
 	animation_player.play("Tell/03_Walk")
 
 func Idle():
+	if IsPlayingIntro == true:
+		return
 	animation_player.play("Tell/01_Idle")
 
 func Sprint():
+	if IsPlayingIntro == true:
+		return
 	animation_player.play("Tell/02_Run")
